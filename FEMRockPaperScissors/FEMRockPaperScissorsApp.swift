2d53f8e478c428e0863f372b5d9952f305bd1c0a
Rachel Radford <rachelradford21@gmail.com>
2022-06-25 11:59:45 -0400

    Initial Commit

---
+++ FEMRockPaperScissors/FEMRockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FEMRockPaperScissorsApp.swift
+//  FEMRockPaperScissors
+//
+//  Created by Rachel Radford on 6/25/22.
+//
+
+import SwiftUI
+
+@main
+struct FEMRockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
